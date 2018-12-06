d9fdd148fb4be270f7beed9c51c56c2eedfec89b
Stewart ilondanga <stilondanga@gmail.com>
2018-12-06 23:32:52 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,29 +19,29 @@ $('.deleteList li').each(function() {
   $(this).children('span').after($('<span />').text(text));
 });
 
-/*$('.deleteList li .delete').on('click', function(e) {
-    $(this).addClass('active hover');
-    var line = $('<div />').addClass('line');
-    var li = $(this).parent();
-    setTimeout(function() {
-        li.append(line);
-        line.animate({
-            width: li.width() - 6
-        }, 400, function() {
-            li.addClass('beforeSlice');
-            line.css({
-                left: 0,
-                right: 'auto'
-            }).animate({
-                width: 0
-            }, 200, function() {
-                li.addClass('slice');
-                setTimeout(function() {
-                    li.slideFadeToggle(300, function() {
-                        li.remove();
-                    });
-                }, 500);
-            });
-        });
-    }, 200);
+$('.deleteList li .delete').on('click', function(e) {
+  $(this).addClass('active hover');
+  var line = $('<div />').addClass('line');
+  var li = $(this).parent();
+  setTimeout(function() {
+    li.append(line);
+    line.animate({
+      width: li.width() - 6
+    }, 400, function() {
+      li.addClass('beforeSlice');
+      line.css({
+        left: 0,
+        right: 'auto'
+      }).animate({
+        width: 0
+      }, 200, function() {
+        li.addClass('slice');
+        setTimeout(function() {
+          li.slideFadeToggle(300, function() {
+            li.remove();
+          });
+        }, 500);
+      });
+    });
+  }, 200);
 });
