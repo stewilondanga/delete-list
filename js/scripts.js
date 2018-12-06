80d2c909bbbe10ec1c3cd5b82f35289c12ea978d
Stewart ilondanga <stilondanga@gmail.com>
2018-12-06 22:08:59 +0300

    add css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,39 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$.fn.slideFadeToggle  = function(speed, easing, callback) {
+    return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+};
+
+$('.deleteList li').each(function() {
+    var text = $(this).children('span').text();
+    $(this).children('span').after($('<span />').text(text));
+});
+
+$('.deleteList li .delete').on('click', function(e) {
+    $(this).addClass('active hover');
+    var line = $('<div />').addClass('line');
+    var li = $(this).parent();
+    setTimeout(function() {
+        li.append(line);
+        line.animate({
+            width: li.width() - 6
+        }, 400, function() {
+            li.addClass('beforeSlice');
+            line.css({
+                left: 0,
+                right: 'auto'
+            }).animate({
+                width: 0
+            }, 200, function() {
+                li.addClass('slice');
+                setTimeout(function() {
+                    li.slideFadeToggle(300, function() {
+                        li.remove();
+                    });
+                }, 500);
+            });
+        });
+    }, 200);
+});
